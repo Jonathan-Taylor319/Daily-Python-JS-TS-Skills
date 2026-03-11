5da11b2df60546e14939cfbd29b302f107c506b5
Jonathan-Taylor319 <jontaylor319@gmail.com>
2026-03-11 11:01:00 -0400

    forgot to push yesterday

--- javascriptTrainingMarch26.js
+++ javascriptTrainingMarch26.js
@@ -1,7 +1,29 @@
-// 3/6/26
+// 3/9/26
+// map()
+
 // problem - convert an array of strings to uppercase
 
-let names = ['jon', 'james', 'cash', 'bobby', 'trixie']
-let cappedNames = names.map(n => n.toUpperCase())
+// let names = ['jon', 'james', 'cash', 'bobby', 'trixie']
+// let cappedNames = names.map(n => n.toUpperCase())
 
-console.log(cappedNames)
+// console.log(cappedNames)
+
+// 3/10/26
+// filter()
+
+//ex:
+// let numbers = [1,2,3,4,5,6]
+// let evens = numbers.filter(n => n % 2 === 0)
+// console.log(evens)
+
+// problem - given an array of words, return an array with only words over 4 letters
+
+// let words = ["cat","house","tree","elephant"]
+
+// let wordsMoreThanFour = words.filter(n => n.length > 4 )
+
+// console.log(wordsMoreThanFour)
+
+// let nums = [1,2,3,4,5,6]
+// let result = nums.filter(number => number > 3 )
+// console.log(result)
--- python_training_march_26.py
+++ python_training_march_26.py
@@ -1,15 +1,49 @@
-# 3/6/26 
+# 3/9/26 
+#collections.counter
+
 # problem - find the most common character in a string using Collections.counter
 
-from collections import Counter
+# from collections import Counter
 
-string1 = 'this is my string'
-string2 = 'this is going to be a longish kind of string to test things'
+# string1 = 'this is my string'
+# string2 = 'this is going to be a longish kind of string to test things'
 
-def find_most_used(text):
-    counts = Counter(text.replace(" ",""))
-    print(counts.most_common(1))
+# def find_most_used(text):
+#     counts = Counter(text.replace(" ",""))
+#     print(counts.most_common(1))
 
-find_most_used(string1)
-find_most_used(string2)
+# find_most_used(string1)
+# find_most_used(string2)
 
+# 3/10/26
+#set()
+
+# #ex:
+# nums = [1,2,2,3,4,4,5]
+# nums2 = [1,2,3,4,5,6,7,8,9]
+# nums3 = [2, 5, 2, 1, 6, 7, 8, 9, 25, 16]
+
+# # unique_nums = set(nums)
+# # print(unique_nums)
+
+# # # membership checking is fast
+# # print(3 in nums)
+
+# # names = ['jon', 'john', 'james', 'jon', 'joseph', 'jon']
+# # names_of_user_no_duplicates = set(names)
+# # print(names_of_user_no_duplicates)
+# # print("jon" in names_of_user_no_duplicates)
+# # print('sam' in names_of_user_no_duplicates)
+
+# # Given a list of numbers return true if a number appears more than once - 
+# # def contains_duplicates(numbers):
+# #     nums_duplicates_removed = set(numbers)
+# #     return len(numbers) != len(nums_duplicates_removed)
+
+# #same as above but refractured 
+# def contains_duplicates(numbers):
+#     return len(numbers) != len(set(numbers))
+
+# print(contains_duplicates(nums))
+# print(contains_duplicates(nums2))
+# print(contains_duplicates(nums3))
